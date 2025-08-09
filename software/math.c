914db47d5723d32adbbe3ab9324bf648e2ab1868
DXdev745 <kumenitbemnet@gmail.com>
2025-08-09 13:04:54 -0400

    Create math.c

---
+++ software/math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n power:%.1f",pow(2,4));
+    
+    printf("\n\n square root:%.1f",sqrt(144));
+    
+    printf("\n\n abs:%d",abs(-144));
+   printf("\n\n round:%.1f",round(144.3));
+    printf("\n\n round:%.1f",round(144.9));
+    
+    printf("\n\n ceil:%.1f",ceil(14.3)); 
+    
+     printf("\n\n floor:%.1f",floor(17.3));
+      printf("\n\n floor:%.1f",floor(17.8));
+    
+    
+    
+}
