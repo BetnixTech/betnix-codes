50ea2990d8bcf4c877e4503c2fc0365ee750246e
DXdev745 <kumenitbemnet@gmail.com>
2025-08-09 13:09:03 -0400

    Create disclaimer.cpp

---
+++ disclaimer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "all of this code in this github resporues is offical betnix properity all of the code is under mit lisense";
+    return 0;
+}
